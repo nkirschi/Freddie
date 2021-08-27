fe46c30400e1a6383e5c22384a26dc1855cb3587
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-08-27 15:52:42 +0200

    Add saving feature

--- harry_plotter.py
+++ harry_plotter.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from matplotlib.dates import DateFormatter
 
+LINEWIDTH = 0.25  # default plot linewidth
 
 # update settings for LaTeX exporting
 mpl.rcParams.update({
@@ -14,17 +15,20 @@ mpl.rcParams.update({
 })
 
 
-def plot_components_and_norm(x, Y, *, symbol, vlines=()):
-    plt.gca().xaxis.set_major_formatter(DateFormatter("%H:%M"))
+def plot_components_and_norm(x, Y, *, symbol, vlines=(), outfile=None):
+    plt.gca().xaxis.set_major_formatter(DateFormatter("%H\\mathord{:}%M"))
 
     for (i, y) in enumerate(Y):
-        plt.plot(x, y, linewidth=0.25, label=f"${symbol}_{i}$")
+        plt.plot(x, y, linewidth=LINEWIDTH, label=f"${symbol}_{i + 1}$")
 
     n = np.linalg.norm(np.array(Y), axis=0)
-    p = plt.plot(x, n, linewidth=0.25, label=f"$\\pm||\\vec{{{symbol}}}||$")
-    plt.plot(x, -n, linewidth=0.25, color=p[-1].get_color())
+    p = plt.plot(x, n, linewidth=LINEWIDTH, label=f"$\\pm||\\vec{{{symbol}}}||$")
+    plt.plot(x, -n, linewidth=LINEWIDTH, color=p[-1].get_color())
 
     for x in vlines:
-        plt.axvline(x, linewidth=0.25, color="#000000")
+        plt.axvline(x, linewidth=LINEWIDTH, color="black")
 
     plt.legend()
+
+    if outfile:
+        plt.savefig(outfile)
