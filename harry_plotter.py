40101b300725a311826b7363d9734dd573ec50e3
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-09-07 00:04:52 +0200

    Improve orbit plot

--- harry_plotter.py
+++ harry_plotter.py
@@ -2,21 +2,25 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
+from itertools import chain, cycle
 from matplotlib.dates import DateFormatter
 
 LINEWIDTH = 0.25  # default plot linewidth
 
 # update settings for LaTeX exporting
 mpl.rcParams.update({
+    "text.usetex": True,
     "pgf.texsystem": "pdflatex",
     "font.family": "serif",
-    "text.usetex": True,
-    "pgf.rcfonts": False
+    "text.latex.preamble": "\n".join([
+        r"\usepackage{palatino}",
+        r"\usepackage{siunitx}",
+    ])
 })
 
 
-def plot_components_and_norm(x, Y, *, symbol, vlines=(), outfile=None):
-    plt.gca().xaxis.set_major_formatter(DateFormatter("%H\\mathord{:}%M"))
+def plot_components_and_norm(x, Y, *, symbol, vlines=(), title=None, xlabel=None, ylabel=None, outfile=None):
+    plt.gca().xaxis.set_major_formatter(DateFormatter("$%d$ %b '$%y$\n$%H$:$%M$", usetex=False))
 
     for (i, y) in enumerate(Y):
         plt.plot(x, y, linewidth=LINEWIDTH, label=f"${symbol}_{i + 1}$")
@@ -25,9 +29,17 @@ def plot_components_and_norm(x, Y, *, symbol, vlines=(), outfile=None):
     p = plt.plot(x, n, linewidth=LINEWIDTH, label=f"$\\pm||\\vec{{{symbol}}}||$")
     plt.plot(x, -n, linewidth=LINEWIDTH, color=p[-1].get_color())
 
-    for x in vlines:
-        plt.axvline(x, linewidth=LINEWIDTH, color="black")
+    colors = ["cyan", "olive", "pink", "gray"]
+    c = cycle(chain(colors, reversed(colors[:-1])))
+    for i, x in enumerate(vlines[:-1]):
+        plt.axvspan(x, vlines[i + 1], alpha=0.5, facecolor=next(c), edgecolor="black", linewidth=0)
 
+    plt.grid(linewidth=LINEWIDTH)
+    plt.margins(x=0)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.xticks(fontsize=8)
     plt.legend()
 
     if outfile:
