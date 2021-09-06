8625d65c19c69aa000342226b43812e86c7f6aa7
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-09-07 00:02:37 +0200

    Implement performance timing utility

---
+++ timer.py
@@ -0,0 +1,46 @@
+import time
+
+
+class Timer:
+    """Timer for code performance measurements."""
+
+    def __init__(self, name=None, text="Elapsed time: {:.6f} seconds"):
+        """
+        Initializes the timer with the given parameters.
+
+        Parameters
+        ----------
+        name : str, default None
+            The display name of this timer.
+        text : str, default "Elapsed time: {:.6f} seconds"
+            A monadic format string for reporting elapsed time.
+        """
+
+        self._time = None
+        self._name = f"[{name}] " if name else ""
+        self._text = text
+
+    def start(self):
+        """Starts the timer immediately."""
+        if self._time is not None:
+            raise ValueError("Timer is already running. You need to .stop() it first!")
+
+        self._time = time.perf_counter()
+
+    def stop(self):
+        """Stops the timer and report elapsed time."""
+        if self._time is None:
+            raise ValueError("Timer is not running. You need to .start() it first!")
+
+        elapsed = time.perf_counter() - self._time
+        self._time = None
+        print(self._name + self._text.format(elapsed))
+
+    def __enter__(self):
+        """Starts timer as context manager."""
+        self.start()
+        return self
+
+    def __exit__(self, *exc_info):
+        """Stops context-managed timer."""
+        self.stop()
