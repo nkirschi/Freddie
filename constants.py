fa72c2518eb99f88ce21de324c79b484037cd185
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-09-07 00:01:40 +0200

    Add data type specification

--- constants.py
+++ constants.py
@@ -15,6 +15,40 @@ DATE_COL = "DATE"
 ORBIT_COL = "ORBIT"
 LABEL_COL = "LABEL"
 
+# memory data types for learning
+DTYPES = {
+    "X_MSO": "float32",
+    "Y_MSO": "float32",
+    "Z_MSO": "float32",
+    "BX_MSO": "float32",
+    "BY_MSO": "float32",
+    "BZ_MSO": "float32",
+    "DBX_MSO": "float32",
+    "DBY_MSO": "float32",
+    "DBZ_MSO": "float32",
+    "RHO_DIPOLE": "float32",
+    "PHI_DIPOLE": "float32",
+    "THETA_DIPOLE": "float32",
+    "BABS_DIPOLE": "float32",
+    "BX_DIPOLE": "float32",
+    "BY_DIPOLE": "float32",
+    "BZ_DIPOLE": "float32",
+    "RHO": "float32",
+    "RXY": "float32",
+    "X": "float32",
+    "Y": "float32",
+    "Z": "float32",
+    "VX": "float32",
+    "VY": "float32",
+    "VZ": "float32",
+    "VABS": "float32",
+    "D": "float32",
+    "COSALPHA": "float32",
+    "EXTREMA": "int8",
+    "ORBIT": "int16",
+    "LABEL": "int8"
+}
+
 # relevant feature column names
 COLS_3D = [
     ["X", "Y", "Z"],
