083e1bb3300a4827c133024786d9f5b79b8dd484
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-09-19 15:35:13 +0200

    Implement train/test split

--- messenger_dataset.py
+++ messenger_dataset.py
@@ -3,7 +3,8 @@ import pandas as pd
 import utils
 
 from constants import *
-from torch.utils.data import Dataset
+from itertools import chain
+from torch.utils.data import Dataset, Subset
 
 
 class MessengerDataset(Dataset):
@@ -12,7 +13,7 @@ class MessengerDataset(Dataset):
     """
 
     # TODO add parameters for 3D and 1D feature column names
-    def __init__(self, orbits_path, *, window_size=8, normalize=True):
+    def __init__(self, orbits_path, *, window_size=8, normalize=True, partial=False):
         """
         Loads the MESSENGER dataset from orbits_path and configures it according to the parameters.
 
@@ -24,9 +25,22 @@ class MessengerDataset(Dataset):
             The size each window shall have.
         normalize : bool, default True
             Whether to normalize the features.
+        partial : bool, default False
+            Whether to only use a small fraction of the data.
         """
 
-        self.data = pd.read_csv(orbits_path)
+        data = pd.read_csv(orbits_path,
+                           index_col=DATE_COL,
+                           parse_dates=True,
+                           dtype=DTYPES,
+                           usecols=USED_COLS,
+                           memory_map=True,
+                           nrows=1e7 if partial else None)
+
+        if partial:  # remove probably incomplete last group
+            data = data[data[ORBIT_COL] != data.iloc[-1][ORBIT_COL]]
+
+        self.data = data
         self.window_size = window_size
         self.skip_size = window_size - 1
 
@@ -56,14 +70,14 @@ class MessengerDataset(Dataset):
 
         Parameters
         ----------
-        idx
+        idx : int
             The index of the desired window.
 
         Returns
         -------
-        sample : bool
+        sample : tensor of float
             The requested window as float tensor.
-        label : int
+        label : list of int
             The corresponding label between 0 and 4.
 
         Raises
@@ -79,8 +93,39 @@ class MessengerDataset(Dataset):
         pos = idx + orbit_skips * self.skip_size
         window = self.data.iloc[pos:(pos + self.window_size)]
 
-        feats_3d = [torch.tensor(window[feat].values) for feat in COLS_3D]
+        feats_3d = [torch.tensor(window[feat].values, dtype=torch.float) for feat in COLS_3D]
         sample = torch.stack(feats_3d, dim=1)  # window_size x #feats_3d x 3
-        label = torch.tensor(window[LABEL_COL].values)  # list of ordinal labels
+        label = torch.tensor(window[LABEL_COL].values, dtype=torch.long)  # list of ordinal labels
 
         return sample, label
+
+    def split(self, holdout_ratio):
+        """
+        Partitons the data into disjoint training and evaluation sets on the orbit level.
+
+        Parameters
+        ----------
+        holdout_ratio : float
+            The percentage of orbits that the evaluation subset shall cover.
+
+        Returns
+        -------
+        (Subset, Subset)
+            Two disjoint subsets of the data.
+        """
+
+        # shuffle the orbits
+        cwn = self.cum_window_nums
+        orbits = torch.randperm(len(cwn))
+
+        # split on the orbit level
+        split_idx = int(holdout_ratio * len(orbits))
+        train_orbits = orbits[:split_idx].tolist()
+        test_orbits = orbits[split_idx:].tolist()
+
+        # split on the index level
+        orbit_range = lambda o: range(cwn.iloc[o - 1] if o > 0 else 0, cwn.iloc[o])
+        train_indices = list(chain.from_iterable(orbit_range(o) for o in train_orbits))
+        test_indices = list(chain.from_iterable(orbit_range(o) for o in test_orbits))
+
+        return Subset(self, train_indices), Subset(self, test_indices)
