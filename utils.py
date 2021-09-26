ff295d1819907c77fb3ec31f82f2b22ace095e12
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-09-26 16:40:17 +0200

    Factor out seeding

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+import random, os
+import numpy as np
+import torch
+
+
 def normalize_decoupled(data, cols):
     data[cols] = (data[cols] - data[cols].mean()) / data[cols].std()
 
@@ -6,3 +11,15 @@ def normalize_coupled(data, cols):
     centered = data[cols] - data[cols].mean()
     distdev = (centered ** 2).sum(axis=1).mean() ** 0.5
     data[cols] = centered / distdev
+
+
+def apply_global_seed(seed: int):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+
+    np.random.seed(seed)
+
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
