c1f50e6dde36ea1e5f8710b216fde6cf27890295
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-10-02 11:44:04 +0200

    Abbreviate useful queries

--- utils.py
+++ utils.py
@@ -13,6 +13,7 @@ def normalize_coupled(data, cols):
     data[cols] = centered / distdev
 
 
+# see https://gist.github.com/ihoromi4/b681a9088f348942b01711f251e5f964
 def apply_global_seed(seed: int):
     random.seed(seed)
     os.environ["PYTHONHASHSEED"] = str(seed)
@@ -23,3 +24,11 @@ def apply_global_seed(seed: int):
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = True
+
+
+def count_trainable_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def is_model_on_gpu(model):
+    return next(model.parameters()).is_cuda
