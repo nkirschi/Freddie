6c1e7483f43354237241250cb122779c1d9a2163
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-10-26 23:21:19 +0200

    Introduce flexible feature choice

--- src/messenger_dataset.py
+++ src/messenger_dataset.py
@@ -1,6 +1,7 @@
 import torch
 import pandas as pd
 import utils
+import os
 
 from constants import *
 from itertools import chain
@@ -9,51 +10,80 @@ from torch.utils.data import Dataset, Subset
 
 class MessengerDataset(Dataset):
     """
-    The MESSENGER dataset, cut into fixed-length time windows.
+    The MESSENGER dataset, cut into fixed-length sliding time windows.
     """
 
-    def __init__(self, orbits_path, *, window_size, normalize, partial, skip_size, start, end):
+    def __init__(self, data_path, *, features, window_size, future_size, use_orbits=None, normalize=True):
         """
         Loads the MESSENGER dataset from orbits_path and configures it according to the parameters.
 
         Parameters
         ----------
-        orbits_path : str
-            The path to the file containing all MESSENGER orbit data.
-        window_size : int, default 8
-            The size each window shall have.
+        data_path : str
+            The path to the folder containing the MESSENGER data.
+        features : list of str
+            The names of features to include in the dataset. Available features:
+            X_MSO, Y_MSO, Z_MSO, BX_MSO, BY_MSO, BZ_MSO, DBX_MSO, DBY_MSO, DBZ_MSO, RHO_DIPOLE, PHI_DIPOLE,
+            THETA_DIPOLE, BABS_DIPOLE, BX_DIPOLE, BY_DIPOLE, BZ_DIPOLE, RHO, RXY, X, Y, Z, VX, VY, VZ, VABS, D, COSALPHA
+        window_size : int
+            The length each window shall have.
+        future_size : int
+            The number of time steps to predict into the future.
+        use_orbits : list of int, default None
+            The IDs of orbits to load. If None, all orbits are loaded.
         normalize : bool, default True
             Whether to normalize the features.
-        partial : bool, default False
-            Whether to only use a small fraction of the data.
         """
 
-        if type(self) == MessengerDataset:
-            raise TypeError("Cannot instantiate abstract class 'MessengerDataset'")
-
-        data = pd.read_csv(orbits_path,
-                           index_col=DATE_COL,
-                           parse_dates=True,
-                           usecols=USED_COLS,
-                           memory_map=True,
-                           nrows=1e6 if partial else None)
-
-        if partial:  # remove probably incomplete last group
-            data = data[data[ORBIT_COL] != data.iloc[-1][ORBIT_COL]]
-
-        self.data = data
+        # initialize attributes
+        self.data_path = data_path
+        self.features = features
         self.window_size = window_size
-        self.skip_size = skip_size
-        self.start = start
-        self.end = end
+        self.future_size = future_size
+        self.use_orbits = use_orbits
+        self.normalize = normalize
 
-        orbit_sizes = self.data.groupby(ORBIT_COL).size()
-        self.cum_window_nums = (orbit_sizes - self.skip_size).cumsum()
+        # load data and metadata
+        self.stats = self.__load_stats()
+        self.class_dist = self.__load_class_dist()
+        self.orbits = self.__load_data()
 
-        if normalize:
-            for feat_3d in COLS_3D:
-                utils.normalize_coupled(self.data, feat_3d)
-            utils.normalize_decoupled(self.data, COLS_SINGLE)
+        # set auxiliary variables
+        self.skip_size = (window_size + future_size) - 1  # how much is missing in the end
+        self.orbit_sizes = pd.Series(len(orbit) for orbit in self.orbits)  # the original lengths of all orbits
+        self.cum_sizes = (self.orbit_sizes - self.skip_size).cumsum()  # the cumulative final orbit lengths
+
+    def __load_stats(self):
+        return pd.read_csv(
+            utils.resolve_path(self.data_path, STATS_FILE),
+            usecols=[STAT_COL].extend(self.features),
+            index_col=STAT_COL
+        )
+
+    def __load_class_dist(self):
+        return pd.read_csv(utils.resolve_path(self.data_path, DIST_FILE), index_col=0)
+
+    def __load_data(self):
+        orbits = []
+        for file in self.__determine_orbits():
+            df_orbit = pd.read_csv(os.path.join(self.data_path, TRAIN_SUBDIR, file),
+                                   usecols=[DATE_COL, LABEL_COL].extend(self.features),
+                                   index_col=DATE_COL,
+                                   parse_dates=True,
+                                   memory_map=True)
+            if self.normalize:
+                data = df_orbit[self.features]
+                mean = self.stats.loc["mean"]
+                std = self.stats.loc["std"]
+                df_orbit.loc[:, self.features] = (data - mean) / std
+            orbits.append(df_orbit)
+        return orbits
+
+    def __determine_orbits(self):
+        if self.use_orbits is None:
+            return sorted(os.listdir(os.path.join(self.data_path, TRAIN_SUBDIR)))
+        else:
+            return map(lambda n: ORBIT_FILE(n), self.use_orbits)
 
     def __len__(self):
         """
@@ -62,14 +92,14 @@ class MessengerDataset(Dataset):
         Returns
         -------
         int
-            The total number of windows.
+            The total number of samples.
         """
 
-        return self.cum_window_nums.iloc[-1]
+        return self.cum_sizes.iloc[-1]
 
     def __getitem__(self, idx):
         """
-        Retrieves the window the given index and its corresponding label.
+        Retrieves the window with the given index and its corresponding label.
 
         Parameters
         ----------
@@ -81,7 +111,7 @@ class MessengerDataset(Dataset):
         sample : tensor of float
             The requested window as float tensor.
         label : list of int
-            The corresponding label between 0 and 4.
+            The corresponding step-wise labels between 0 and 4.
 
         Raises
         ------
@@ -92,13 +122,13 @@ class MessengerDataset(Dataset):
         if idx < 0 or idx >= len(self):
             raise IndexError(f"Index {idx} is out of bounds.")
 
-        orbit_skips = (self.cum_window_nums <= idx).sum()
-        pos = idx + orbit_skips * self.skip_size
-        window = self.data.iloc[pos:(pos + self.window_size)]
-        target = self.data.iloc[(pos + self.start):(pos + self.end)]
+        orbit = (idx >= self.cum_sizes).sum()                        # orbit index of the requested window
+        pos = idx - pd.Series(0).append(self.cum_sizes).iloc[orbit]  # window index within the orbit
 
-        feats_3d = [torch.tensor(window[feat].values, dtype=torch.float) for feat in COLS_3D]
-        sample = torch.stack(feats_3d, dim=1)  # window_size x #feats_3d x 3
+        window = self.orbits[orbit].iloc[pos:(pos + self.window_size)]
+        target = self.orbits[orbit].iloc[pos:(pos + self.window_size + self.future_size)]
+
+        sample = torch.tensor(window[self.features].values, dtype=torch.float).transpose(0, 1)
         label = torch.tensor(target[LABEL_COL].values, dtype=torch.long)
 
         return sample, label
@@ -119,7 +149,7 @@ class MessengerDataset(Dataset):
         """
 
         # shuffle the orbits
-        cwn = self.cum_window_nums
+        cwn = self.cum_sizes
         orbits = torch.randperm(len(cwn))
 
         # split on the orbit level
@@ -134,196 +164,5 @@ class MessengerDataset(Dataset):
 
         return Subset(self, train_indices), Subset(self, test_indices)
 
-
-class ClassificationDataset(MessengerDataset):
-    def __init__(self, orbits_path, *, window_size, normalize=True, partial=False):
-        super().__init__(orbits_path,
-                         window_size=window_size,
-                         normalize=normalize,
-                         partial=partial,
-                         skip_size=window_size - 1,
-                         start=0,
-                         end=window_size)
-
-    def __getitem__(self, idx):
-        """
-        Retrieves the window the given index and its corresponding label.
-
-        Parameters
-        ----------
-        idx : int
-            The index of the desired window.
-
-        Returns
-        -------
-        sample : 3-d tensor of float
-            The requested window as float tensor.
-        label : 1-d tensor of int
-            The corresponding label between 0 and 4.
-
-        Raises
-        ------
-        IndexError
-            If idx is negative or larger than the dataset size.
-        """
-        return super().__getitem__(idx)
-
-
-class PredictionDataset(MessengerDataset):
-    def __init__(self, orbits_path, *, window_size, normalize=True, partial=False):
-        super().__init__(orbits_path,
-                         window_size=window_size,
-                         normalize=normalize,
-                         partial=partial,
-                         skip_size=window_size,
-                         start=window_size,
-                         end=window_size + 1)
-
-    def __getitem__(self, idx):
-        """
-        Retrieves the window the given index and its corresponding label.
-
-        Parameters
-        ----------
-        idx : int
-            The index of the desired window.
-
-        Returns
-        -------
-        sample : 3-d tensor of float
-            The requested window as float tensor.
-        label : 0-d tensor of int
-            The label of the next time step between 0 and 4.
-
-        Raises
-        ------
-        IndexError
-            If idx is negative or larger than the dataset size.
-        """
-        return super().__getitem__(idx)
-
-        
-# class OldMessengerDataset(Dataset):
-#     """
-#     The MESSENGER dataset, cut into fixed-length time windows.
-#     """
-#
-#     def __init__(self, orbits_path, *, window_size=8, normalize=True, partial=False, predict=False):
-#         """
-#         Loads the MESSENGER dataset from orbits_path and configures it according to the parameters.
-#
-#         Parameters
-#         ----------
-#         orbits_path : str
-#             The path to the file containing all MESSENGER orbit data.
-#         window_size : int, default 8
-#             The size each window shall have.
-#         normalize : bool, default True
-#             Whether to normalize the features.
-#         partial : bool, default False
-#             Whether to only use a small fraction of the data.
-#         """
-#
-#         data = pd.read_csv(orbits_path,
-#                            index_col=DATE_COL,
-#                            parse_dates=True,
-#                            usecols=USED_COLS,
-#                            memory_map=True,
-#                            nrows=1e6 if partial else None)
-#
-#         if partial:  # remove probably incomplete last group
-#             data = data[data[ORBIT_COL] != data.iloc[-1][ORBIT_COL]]
-#
-#         self.data = data
-#         self.window_size = window_size
-#         self.skip_size = window_size - (0 if predict else 1)
-#         self.predict = predict
-#
-#         orbit_sizes = self.data.groupby(ORBIT_COL).size()
-#         self.cum_window_nums = (orbit_sizes - self.skip_size).cumsum()
-#
-#         if normalize:
-#             for feat_3d in COLS_3D:
-#                 utils.normalize_coupled(self.data, feat_3d)
-#             utils.normalize_decoupled(self.data, COLS_SINGLE)
-#
-#     def __len__(self):
-#         """
-#         Yields the size of the dataset.
-#
-#         Returns
-#         -------
-#         int
-#             The total number of windows.
-#         """
-#
-#         return self.cum_window_nums.iloc[-1]
-#
-#     def __getitem__(self, idx):
-#         """
-#         Retrieves the window the given index and its corresponding label.
-#
-#         Parameters
-#         ----------
-#         idx : int
-#             The index of the desired window.
-#
-#         Returns
-#         -------
-#         sample : tensor of float
-#             The requested window as float tensor.
-#         label : list of int
-#             The corresponding label between 0 and 4.
-#
-#         Raises
-#         ------
-#         IndexError
-#             If idx is negative or larger than the dataset size.
-#         """
-#
-#         if idx < 0 or idx >= len(self):
-#             raise IndexError(f"Index {idx} is out of bounds.")
-#
-#         orbit_skips = (self.cum_window_nums <= idx).sum()
-#         pos = idx + orbit_skips * self.skip_size
-#         window = self.data.iloc[pos:(pos + self.window_size)]
-#
-#         feats_3d = [torch.tensor(window[feat].values, dtype=torch.float) for feat in COLS_3D]
-#         sample = torch.stack(feats_3d, dim=1)  # window_size x #feats_3d x 3
-#         if self.predict:  # ordinal label of next time step
-#             label = torch.tensor(self.data.iloc[pos + self.window_size], dtype=torch.long)
-#         else:  # list of ordinal labels for each time step within the window
-#             label = torch.tensor(window[LABEL_COL].values, dtype=torch.long)
-#
-#         return sample, label
-#
-#     def split(self, holdout_ratio):
-#         """
-#         Partitons the data into disjoint training and evaluation sets on the orbit level.
-#
-#         Parameters
-#         ----------
-#         holdout_ratio : float
-#             The percentage of orbits that the evaluation subset shall cover.
-#
-#         Returns
-#         -------
-#         (Subset, Subset)
-#             Two disjoint subsets of the data.
-#         """
-#
-#         # shuffle the orbits
-#         cwn = self.cum_window_nums
-#         orbits = torch.randperm(len(cwn))
-#
-#         # split on the orbit level
-#         split_idx = int(holdout_ratio * len(orbits))
-#         train_orbits = orbits[:split_idx].tolist()
-#         test_orbits = orbits[split_idx:].tolist()
-#
-#         # split on the index level
-#         orbit_range = lambda o: range(cwn.iloc[o - 1] if o > 0 else 0, cwn.iloc[o])
-#         train_indices = list(chain.from_iterable(orbit_range(o) for o in train_orbits))
-#         test_indices = list(chain.from_iterable(orbit_range(o) for o in test_orbits))
-#
-#         return Subset(self, train_indices), Subset(self, test_indices)
+    def get_class_distribution(self):
+        return torch.tensor(self.class_dist.values, dtype=torch.float32)
