3bed257c9c0338a8e639a105005f5812c1c38ff2
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-10-26 23:23:54 +0200

    Adjust data directories

--- src/constants.py
+++ src/constants.py
@@ -1,51 +1,37 @@
 # relevant directories !relative to project root!
-LABEL_DIR = "labels"    # where the label mapping is located
-ORBIT_DIR = "orbits"    # where the orbit data is located
-DATA_DIR = "data"       # where the final training data resides
-FIGURE_DIR = "figures"  # where figures are saved
-MODELS_DIR = "models"   # where the trained models reside
+DATA_DIR     = "data"         # where metadata about data is located
+LABEL_SUBDIR = "labels"       # where the label mapping is located
+RAW_SUBDIR   = "raw"          # where the raw orbit data is located
+TRAIN_SUBDIR = "train"        # where the final training data resides
+
+TEMP_DIR     = "temp"         # where temporary content is saved
+RUNS_DIR     = "runs"         # where the training run data resides
+MODELS_DIR   = "models"       # where the trained models reside
 
 # important file names
 MESSENGER = lambda n: f"messenger-{n:04d}"
 ORBIT_FILE = lambda n: MESSENGER(n) + ".csv"
 LABEL_FILE = "messenger-0001_-4094_labelled.csv"
-TRAIN_FILE = "df_train.csv"
+STATS_FILE = "statistics.csv"
+DIST_FILE = "class_distribution.csv"
+HEALTH_FILE = "orbit_health.csv"
+HPARAMS_FILE = "hyperparams.json"
+TPARAMS_FILE = "techparams.json"
 
 # important column names
 DATE_COL = "DATE"
 ORBIT_COL = "ORBIT"
 LABEL_COL = "LABEL"
+STAT_COL = "STAT"
 
-# event columns in label file
 EVENT_COLS = list(range(1, 9))
-
-# relevant feature column names
-COLS_3D = [
-    ["BX_MSO", "BY_MSO", "BZ_MSO"],
-    ["X_MSO", "Y_MSO", "Z_MSO"],
-    #["X", "Y", "Z"],
-    #["VX", "VY", "VZ"],
-    #["DBX_MSO", "DBY_MSO", "DBZ_MSO"],
-    #["BX_DIPOLE", "BY_DIPOLE", "BZ_DIPOLE"],
-]
-COLS_SINGLE = [
-    #"RHO_DIPOLE",
-    #"PHI_DIPOLE",
-    #"THETA_DIPOLE",
-    #"BABS_DIPOLE",
-    #"RHO", "RXY",
-    #"VABS", "D",
-    #"COSALPHA"
-]
-FLUX_COLS = COLS_3D[0]
-PRED_COLS = [DATE_COL] + COLS_SINGLE + sum(COLS_3D, [])
-USED_COLS = [ORBIT_COL, LABEL_COL] + PRED_COLS
+FLUX_COLS = ["BX_MSO", "BY_MSO", "BZ_MSO"]
 
 # class name mapping
 CLASSES = {
-    0: "interplanetary magnetic field",
-    1: "bow shock crossing",
+    0: "interplanetary magnetic field (IMF)",
+    1: "bow shock crossing (SK)",
     2: "magnetosheath",
-    3: "magnetopause crossing",
+    3: "magnetopause crossing (MP)",
     4: "magnetosphere"
 }
