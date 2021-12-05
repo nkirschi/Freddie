f614f0a9e719bf1dac2109360544bf7d98640533
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-12-06 00:00:28 +0100

    Add new models

--- src/models.py
+++ src/models.py
@@ -1,120 +1,247 @@
+"""
+This file contains definitions of deep learning models for the Freddie task.
+All models inherit from the base class 'FreddieModel'. They expect an input
+sample of shape (batch_size, num_channels, window_size) and yield an output
+prediction of shape (batch_size, num_classes, window_size + future_size).
+"""
+
+__author__ = "Nikolas Kirschstein"
+__copyright__ = "Copyright 2021, Nikolas Kirschstein, All rights reserved."
+__license__ = "Apache License 2.0"
+__version__ = "1.0.0"
+__maintainer__ = "Nikolas Kirschstein"
+__email__ = "nikolas.kirschstein@gmail.com"
+__status__ = "Prototype"
+
 import torch.nn as nn
-import torch.nn.functional as F
+
+from abc import ABC, abstractmethod
+from utils.swap_last import SwapLast
 
 
-class BaseNet(nn.Module):
+################################################################################
+
+class FreddieModel(nn.Module, ABC):
     """
-    Baseline model with a couple of linear layers and ReLU activations.
+    Base class for all models.
+
+    Inheriting from this class provides predefined attributes for the input and output shape, respectively.
     """
 
-    def __init__(self, num_channels, window_size, future_size, hidden_sizes, num_classes=5):
+    def __init__(self, num_channels, window_size, future_size, num_classes):
         super().__init__()
 
-        in_shape = (num_channels, window_size)
-        out_shape = (num_classes, window_size + future_size)
+        self.in_shape = (num_channels, window_size)
+        self.out_shape = (num_classes, window_size + future_size)
+
+    @abstractmethod
+    def forward(self, x):
+        ...
+
+
+################################################################################
+
+# TODO move these utility methods to a separate file
+
+def build_linear_stack(in_size, out_size, hidden_sizes, dropout_rate):
+    linear_stack = []
+    hidden_sizes = [in_size] + hidden_sizes
+    for i in range(len(hidden_sizes) - 1):
+        linear_stack.append(nn.Linear(hidden_sizes[i], hidden_sizes[i + 1]))
+        if dropout_rate > 0:
+            linear_stack.append(nn.Dropout(dropout_rate))
+        linear_stack.append(nn.ReLU())
+    linear_stack.append(nn.Linear(hidden_sizes[-1], out_size))
+    return nn.Sequential(*linear_stack)
+
+
+def build_conv_stack(in_size, channel_sizes, kernel_sizes, stride_sizes, pool_sizes, dropout_rate):
+    conv_stack = []
+    channel_sizes = [in_size] + channel_sizes
+    for i in range(len(channel_sizes) - 1):
+        conv_stack.append(nn.Conv1d(channel_sizes[i], channel_sizes[i + 1],
+                                    kernel_size=(kernel_sizes[i],),
+                                    stride=(stride_sizes[i],),
+                                    padding=kernel_sizes[i] // 2))
+        if dropout_rate > 0:
+            conv_stack.append(nn.Dropout(dropout_rate))
+        conv_stack.append(nn.ReLU())
+        if pool_sizes[i] > 1:
+            conv_stack.append(nn.MaxPool1d(pool_sizes[i]))
+    return nn.Sequential(*conv_stack)
+
+
+def determine_conv_out_size(initial_size, kernel_sizes, stride_sizes, pool_sizes):
+    if len(kernel_sizes) == 0:
+        return initial_size
+
+    in_size = determine_conv_out_size(initial_size, kernel_sizes[:-1], stride_sizes[:-1], pool_sizes[:-1])
+    return ((in_size - kernel_sizes[-1] % 2) // stride_sizes[-1] + 1) // pool_sizes[-1]
+
+
+################################################################################
+
+class MLP(FreddieModel):
+    """
+    Simple multi-layer perceptron with ReLU activations and optional dropout.
+    """
+
+    def __init__(self, num_channels, window_size, future_size, num_classes=5, **kwargs):
+        super().__init__(num_channels, window_size, future_size, num_classes)
 
         self.flatten = nn.Flatten(-2, -1)
-        self.linear_in = nn.Linear(in_shape[0] * in_shape[1], hidden_sizes[0])
-        self.relu = nn.ReLU()
-        hidden_stack = []
-        for i in range(len(hidden_sizes) - 1):
-            hidden_stack.append(nn.Linear(hidden_sizes[i], hidden_sizes[i + 1]))
-            hidden_stack.append(nn.ReLU())
-        self.hidden_stack = nn.Sequential(*hidden_stack)
-        self.linear_out = nn.Linear(hidden_sizes[-1], out_shape[0] * out_shape[1])
-        self.unflatten = nn.Unflatten(-1, out_shape)
+        self.hidden_stack = build_linear_stack(self.in_shape[0] * self.in_shape[1],
+                                               self.out_shape[0] * self.out_shape[1],
+                                               kwargs["hidden_sizes"],
+                                               kwargs["dropout_rate"])
+        self.unflatten = nn.Unflatten(-1, self.out_shape)
 
     def forward(self, x):
         x = self.flatten(x)
-        x = self.linear_in(x)
-        x = self.relu(x)
         x = self.hidden_stack(x)
-        x = self.linear_out(x)
         x = self.unflatten(x)
 
         return x
 
 
-class BaseCNN(nn.Module):
+################################################################################
+
+class CNN(FreddieModel):
     """
-    Baseline CNN for window classification.
+    Standard CNN with 1D convolutions, optional pooling and optional dropout.
     """
 
-    def __init__(self, *, window_size, num_channels, num_classes=5, input_dim=3):
-        super().__init__()
+    def __init__(self, num_channels, window_size, future_size, num_classes=5, **kwargs):
+        super().__init__(num_channels, window_size, future_size, num_classes)
 
-        self.conv1 = nn.Conv1d(num_channels * input_dim, 32, 1)
-        self.conv2 = nn.Conv1d(32, 64, 1)
-        self.conv3 = nn.Conv1d(64, 128, 1)
-        self.fc1 = nn.Linear(window_size * 128, 256)
-        self.fc2 = nn.Linear(256, window_size * num_classes)
+        conv_out_size = determine_conv_out_size(window_size,
+                                                kwargs["kernel_sizes"],
+                                                kwargs["stride_sizes"],
+                                                kwargs["pool_sizes"])
+
+        self.conv_stack = build_conv_stack(num_channels,
+                                           kwargs["channel_sizes"],
+                                           kwargs["kernel_sizes"],
+                                           kwargs["stride_sizes"],
+                                           kwargs["pool_sizes"],
+                                           kwargs["dropout_rate"])
+        self.flatten = nn.Flatten(-2, -1)
+        self.linear_stack = build_linear_stack(kwargs["channel_sizes"][-1] * conv_out_size,
+                                               self.out_shape[0] * self.out_shape[1],
+                                               kwargs["hidden_sizes"],
+                                               kwargs["dropout_rate"])
+        self.unflatten = nn.Unflatten(-1, self.out_shape)
 
     def forward(self, x):
-        x = x.flatten(-2, -1)
-        x = x.transpose(1, 2)
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = self.conv3(x)
-        x = F.relu(x)
-        x = x.flatten(-2, -1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        return x.view(-1, 5, 10)
-
-
-class SimpleCNN(nn.Module):
-    """
-    3 layer simple CNN followed by 2 FC layers.
-    """
-
-    def __init__(self, num_bands, input_dim=3):
-        super().__init__()
-        self.num_bands = num_bands
-        self.conv1 = nn.Conv1d(input_dim, 32, 1)
-        self.conv2 = nn.Conv1d(32, 64, 1)
-        self.conv3 = nn.Conv1d(64, 128, 1)
-        self.fc1 = nn.Linear(128 * self.num_bands, 64)
-        self.fc2 = nn.Linear(64, 5)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = x.reshape(-1, 128 * self.num_bands)
-        x = self.fc1(x)
-        x = self.fc2(x)
+        x = self.conv_stack(x)
+        x = self.flatten(x)
+        x = self.linear_stack(x)
+        x = self.unflatten(x)
 
         return x
 
 
-class SimpleBatchNormedCNN(nn.Module):
+################################################################################
+
+
+class FCN(FreddieModel):
     """
-    3 layer simple batch-normed CNN followed by 3 FC layers.
+    Fully convolutional network with global average pooling (GAP) instead of linear layers.
     """
 
-    def __init__(self, num_bands, input_dim=3):
-        super(SimpleBatchNormedCNN, self).__init__()
-        self.num_bands = num_bands
-        self.conv1 = nn.Conv1d(input_dim, 32, 1)
-        self.bn1 = nn.BatchNorm1d(32)
-        self.conv2 = nn.Conv1d(32, 64, 1)
-        self.bn2 = nn.BatchNorm1d(64)
-        self.conv3 = nn.Conv1d(64, 128, 1)
-        self.bn3 = nn.BatchNorm1d(128)
-        self.fc1 = nn.Linear(128, 64)
-        self.fc2 = nn.Linear(64, 32)
-        self.fc3 = nn.Linear(32, 5)
+    def __init__(self, num_channels, window_size, future_size, num_classes=5, **kwargs):
+        super().__init__(num_channels, window_size, future_size, num_classes)
+
+        self.conv_stack = build_conv_stack(num_channels,
+                                           kwargs["channel_sizes"],
+                                           kwargs["kernel_sizes"],
+                                           kwargs["stride_sizes"],
+                                           kwargs["pool_sizes"],
+                                           kwargs["dropout_rate"])
+        self.out_conv = nn.Conv1d(kwargs["channel_sizes"][-1],
+                                  self.out_shape[0] * self.out_shape[1],
+                                  kernel_size=(1,))
+        self.gap = nn.AvgPool1d(determine_conv_out_size(window_size,
+                                                        kwargs["kernel_sizes"],
+                                                        kwargs["stride_sizes"],
+                                                        kwargs["pool_sizes"]))
+        self.unflatten = nn.Unflatten(-1, self.out_shape)
 
     def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        x = x.transpose(1, 2)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.conv_stack(x)
+        x = self.out_conv(x)
+        x = self.gap(x)[..., 0]
+        x = self.unflatten(x)
+
+        return x
+
+
+################################################################################
+
+class RNN(FreddieModel):
+    """
+    Basic RNN consisting of a stack of long short-term memory (LSTM) modules.
+    """
+
+    def __init__(self, num_channels, window_size, future_size, num_classes=5, **kwargs):
+        super().__init__(num_channels, window_size, future_size, num_classes)
+
+        self.zero_pad = nn.ConstantPad1d((0, future_size), 0)
+        self.swap_in = SwapLast()
+        self.lstm = nn.LSTM(input_size=num_channels,
+                            hidden_size=kwargs["rnn_state_size"],
+                            bidirectional=True,
+                            batch_first=True,
+                            num_layers=kwargs["rnn_layers"],
+                            dropout=kwargs["dropout_rate"]
+                            )
+        self.fc = nn.Linear(2 * kwargs["rnn_state_size"], num_classes)
+        self.swap_out = SwapLast()
+
+    def forward(self, x):
+        x = self.zero_pad(x)
+        x = self.swap_in(x)
+        x = self.lstm(x)[0]
+        x = self.fc(x)
+        x = self.swap_out(x)
+
+        return x
+
+
+################################################################################
+
+class CRNN(FreddieModel):
+    """
+    Convolutional recurrent network combining convolutions and LSTM components.
+    """
+
+    def __init__(self, num_channels, window_size, future_size, num_classes=5, **kwargs):
+        super().__init__(num_channels, window_size, future_size, num_classes)
+
+        self.conv_stack = build_conv_stack(num_channels,
+                                           kwargs["channel_sizes"],
+                                           kwargs["kernel_sizes"],
+                                           kwargs["stride_sizes"],
+                                           kwargs["pool_sizes"],
+                                           kwargs["dropout_rate"])
+        self.zero_pad = nn.ConstantPad1d((0, future_size), 0)
+        self.swap_last1 = SwapLast()
+        self.lstm = nn.LSTM(input_size=kwargs["channel_sizes"][-1],
+                            hidden_size=kwargs["rnn_state_size"],
+                            bidirectional=True,
+                            batch_first=True,
+                            num_layers=kwargs["rnn_layers"],
+                            dropout=kwargs["dropout_rate"]
+                            )
+        self.fc = nn.Linear(2 * kwargs["rnn_state_size"], num_classes)
+        self.swap_last2 = SwapLast()
+
+    def forward(self, x):
+        x = self.conv_stack(x)
+        x = self.zero_pad(x)
+        x = self.swap_last1(x)
+        x = self.lstm(x)[0]
+        x = self.fc(x)
+        x = self.swap_last2(x)
 
         return x
