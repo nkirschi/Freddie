cf77166c7b5f91db6f023a5c78af1e0a188b076d
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-12-05 23:52:52 +0100

    Document metric wrapper

--- src/auprc.py
+++ src/auprc.py
@@ -1,8 +1,27 @@
+"""
+This file wraps a metric for more convenient use.
+"""
+
+__author__ = "Nikolas Kirschstein"
+__copyright__ = "Copyright 2021, Nikolas Kirschstein, All rights reserved."
+__license__ = "Apache License 2.0"
+__version__ = "1.0.0"
+__maintainer__ = "Nikolas Kirschstein"
+__email__ = "nikolas.kirschstein@gmail.com"
+__status__ = "Prototype"
+
+
 import torch
 from torchmetrics import AveragePrecision
 
 
 class AUPRC(AveragePrecision):
+    """
+    Wrapper for the AveragePrecision metric from torchmetrics.
+
+    Its 'compute' method returns the class-wise results as tensor and not a list, therefore complying with the
+    overall convention in the library.
+    """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
