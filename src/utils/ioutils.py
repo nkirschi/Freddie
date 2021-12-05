f07b75c91f0d2e2d5bbe84c19e799c09039229c1
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-12-05 23:48:40 +0100

    Document utility classes

--- src/utils/ioutils.py
+++ src/utils/ioutils.py
@@ -1,6 +1,18 @@
+"""
+Utility functions for working with the file system, terminal etc.
+"""
+
+__author__ = "Nikolas Kirschstein"
+__copyright__ = "Copyright 2021, Nikolas Kirschstein, All rights reserved."
+__license__ = "Apache License 2.0"
+__version__ = "1.0.0"
+__maintainer__ = "Nikolas Kirschstein"
+__email__ = "nikolas.kirschstein@gmail.com"
+__status__ = "Prototype"
+
+
 import os
 import ast
-import sys
 import yaml
 
 from pathlib import Path
@@ -10,14 +22,49 @@ import constants as const
 
 
 def resolve_path(path: PathLike) -> Path:
+    """
+    Resolves the given path relative to the project root.
+
+    Parameters
+    ----------
+    path: PathLike
+        A path relative to the project root.
+
+    Returns
+    -------
+        The resolved absolute path of the target on the file system.
+    """
     return Path(__file__).parent.parent.parent.joinpath(path)
 
 
 def ensure_directory(path: PathLike) -> None:
+    """
+    Ensures that the directory resembled by the given path exists.
+
+    Parameters
+    ----------
+    path: PathLike
+        The path of the concerning directory.
+    """
     Path(path).mkdir(parents=True, exist_ok=True)
 
 
-def create_run_directory(hparams):
+def create_run_directory(hparams: dict) -> tuple:
+    """
+    Sets up a new run directory and stores the given hyperparameters there.
+
+    Parameters
+    ----------
+    hparams: dict
+        A dictionary of hyperparameters to store in the new run directory.
+
+    Returns
+    -------
+    run_path : str
+        The path to the newly created directory on the file system.
+    next_id : int
+        The numeric ID of the new run.
+    """
     path = resolve_path(const.RUNS_DIR)               # runs directory
     run_ids = list(map(int, next(os.walk(path))[1]))  # subfolders as ints
     next_id = (max(run_ids) + 1) if run_ids else 0    # highest id plus one
@@ -31,12 +78,37 @@ def create_run_directory(hparams):
     return run_path, next_id
 
 
-def parse_cli_keyword_args():
-    keyvalpairs = [x.lstrip("--").split("=") for x in sys.argv[1:]]
+def parse_cli_keyword_args(args: list) -> dict:
+    """
+    Parses command line keyword arguments that were given in the format '--key=value'.
+
+    Parameters
+    ----------
+    args : list
+        A list of command line arguments in the described format.
+
+    Returns
+    -------
+    dict
+        The parsed command line arguments as a dictionary.
+    """
+    keyvalpairs = [x.lstrip("--").split("=") for x in args]
     return {key: parse_type(val) for key, val in keyvalpairs}
 
 
-def parse_type(string):
+def parse_type(string: str) -> object:
+    """
+    Casts the given string to the best suited type.
+
+    Parameters
+    ----------
+    string : str
+        The string to interpret as python object.
+
+    Returns
+    -------
+        The casted version of the string or itself if no casting was applicable.
+    """
     try:
         return ast.literal_eval(string)
     except (ValueError, SyntaxError):
--- src/utils/torchutils.py
+++ src/utils/torchutils.py
@@ -1,13 +1,28 @@
+"""
+Utility functions abbreviating common PyTorch patterns.
+"""
+
+__author__ = "Nikolas Kirschstein"
+__copyright__ = "Copyright 2021, Nikolas Kirschstein, All rights reserved."
+__license__ = "Apache License 2.0"
+__version__ = "1.0.0"
+__maintainer__ = "Nikolas Kirschstein"
+__email__ = "nikolas.kirschstein@gmail.com"
+__status__ = "Prototype"
+
+
 import random
 import os
 import numpy as np
 import torch
 
-from torch.nn import DataParallel
-
 
 # see https://gist.github.com/ihoromi4/b681a9088f348942b01711f251e5f964
 def apply_global_seed(seed: int):
+    """
+    Applies the same seed to all libraries that make use of randomness and enforces deterministic GPU computation.
+    """
+
     os.environ["PYTHONHASHSEED"] = str(seed)
 
     random.seed(seed)
@@ -19,10 +34,18 @@ def apply_global_seed(seed: int):
     torch.backends.cudnn.benchmark = True
 
 
-def count_trainable_parameters(model):
+def count_trainable_params(model):
+    """
+    Counts the number of trainable parameters in the given model.
+    """
+
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 
-def is_model_on_gpu(model):
-    return next(model.parameters()).is_cuda
+def get_model_device(model):
+    """
+    Determines which device the given model is currently on.
+    """
+
+    return next(model.parameters()).device
 
