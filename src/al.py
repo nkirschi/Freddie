071387f2aab1cf01152dfcb46cb029568b98f914
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-12-16 11:51:59 +0100

    Improve performance by precomputing critical window tensors

--- src/al.py
+++ src/al.py
@@ -1,3 +1,16 @@
+"""
+This is the active learning script.
+"""
+
+__author__ = "Nikolas Kirschstein"
+__copyright__ = "Copyright 2021, Nikolas Kirschstein, All rights reserved."
+__license__ = "Apache License 2.0"
+__version__ = "1.0.0"
+__maintainer__ = "Nikolas Kirschstein"
+__email__ = "nikolas.kirschstein@gmail.com"
+__status__ = "Prototype"
+
+
 import constants as c
 import random
 import heapq
@@ -13,53 +26,47 @@ def confidence(p: torch.Tensor, dim: int = -1) -> torch.Tensor:
 
 
 def entropy(p: torch.Tensor, dim: int = -1) -> torch.Tensor:
-    return torch.where(p > 0, p * p.log(), p.new([0.])).sum(dim=dim)
+    return -torch.where(p > 0, p * p.log(), p.new([0.])).sum(dim=dim)
 
 
 def orbit_confidence(preds: torch.Tensor):
-    return float(entropy(softmax(preds, dim=1), dim=1).min(dim=1).values.mean())
+    return float(entropy(softmax(preds, dim=1), dim=1).max(dim=1).values.mean())
 
 
 hparams, tparams = training.load_config()
 torchutils.apply_global_seed(hparams["seed"])
 
-
 ds = training.load_dataset(hparams)
-orbits = ds.explode_orbits()
+orbits = {k: torch.stack([sample for sample, label in v if 1 in label or 3 in label]) for k, v in ds.explode_orbits().items()}
 model = training.construct_model(hparams)
 
 # start with single random orbit
-train_orbits = [random.choice(list(ds.get_orbits().keys()))]
+train_orbits = [random.choice(list(orbits.keys()))]
+start_index = 0
 
 increment = 1
-done = False
 
-while not done:
+while len(train_orbits) < 0.7 * len(orbits):
     print(f"train_orbits: {train_orbits}")
-    ds_train, ds_eval = ds.split(train_orbits)
 
-    if len(ds_eval) == 0:
-        break
-
-    dl_train, dl_eval = training.prepare_dataloaders(ds_train, ds_eval, hparams, tparams)
-    #training.perform_train(model, ds, dl_train, dl_eval, hparams, tparams)
+    hparams["train_split"] = train_orbits[start_index:]
+    # training.perform_train(model, ds, hparams, tparams)
     # now model should have params from lowest loss
 
-    ds_orbits = {k: v for (k, v) in orbits.items() if k not in train_orbits}
+    unseen_orbits = {k: v for (k, v) in orbits.items() if k not in train_orbits}
 
-    conf_vals = []
-    for key, ds_orbit in ds_orbits.items():
-        with Timer("stacking"):
-            all_windows = torch.stack([sample for sample, label in ds_orbit])
-        with Timer("inferring"):
-            conf = orbit_confidence(model(all_windows))
-        heapq.heappush(conf_vals, (conf, key))
+    conf_vals = {}
+    for key, orbit_tensor in unseen_orbits.items():
+        with torch.no_grad():
+            preds = model(orbit_tensor)
+        conf = orbit_confidence(preds)
+        conf_vals[key] = conf
 
         print(f"confidence on orbit #{key}:", conf)
+        print(f"-> number of relevant samples: {len(orbit_tensor)}")
 
-    # top_indices = torch.topk(torch.tensor(conf_vals), increment).indices
-    # keys = list(ds_orbits.keys())
-    # most_inconfident_orbits = [keys[i] for i in top_indices]
-    most_inconfident_orbits = [key for _, key in conf_vals[:increment]]
-    print(f"most inconfident orbits: {most_inconfident_orbits}")
-    train_orbits += most_inconfident_orbits
+    start_index = len(train_orbits)
+    train_orbits += heapq.nlargest(increment, conf_vals, conf_vals.get)
+
+    print(f"most inconfident orbits: {train_orbits[start_index:]}")
+
