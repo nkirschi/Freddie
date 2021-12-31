fd328744164b021640e4a058c17ee86d28014f8a
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-12-31 18:59:02 +0100

    Add function plotting function

--- src/utils/harry_plotter.py
+++ src/utils/harry_plotter.py
@@ -20,14 +20,16 @@ from itertools import chain, cycle, product
 from matplotlib.dates import DateFormatter
 from matplotlib.patches import Patch
 
-LINEWIDTH = 0.25  # default plot linewidth
+LINEWIDTH_THIN = 0.25  # thin plot linewidth
+LINEWIDTH_THICK = 1  # thick plot linewidth
+LINESTYLE = "--"  # default line style
 
 # update settings for LaTeX exporting
 mpl.rcParams.update({
     "text.usetex": True,
     "pgf.texsystem": "pdflatex",
     "font.family": "serif",
-    "font.size": 12,
+    "font.size": 11,
     "figure.autolayout": True,
     "text.latex.preamble": "\n".join([
         r"\usepackage{palatino}",
@@ -36,12 +38,26 @@ mpl.rcParams.update({
 })
 
 
+def plot_function(func, xlim, ylim=(), label=None, xlabel=None, ylabel=None, title=None, outfile=None, **kwargs):
+    x = np.arange(xlim[0], xlim[1] + (xlim[1] - xlim[0]) / 100, (xlim[1] - xlim[0]) / 100)
+
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    if ylim:
+        plt.ylim(ylim)
+    plt.grid(linewidth=LINEWIDTH_THIN, linestyle=LINESTYLE)
+    plt.plot(x, func(x), label=label, linewidth=LINEWIDTH_THICK, **kwargs)
+
+    _save_figure(outfile)
+
+
 def plot_metrics(train_metric, eval_metric, ylabel=None, outfile=None):
     plt.figure(figsize=(4, 3))
     plt.plot(range(1, len(train_metric) + 1), train_metric, label="training")
     plt.plot(range(1, len(eval_metric) + 1), eval_metric, label="validation")
     plt.legend()
-    plt.grid(linestyle="--")
+    plt.grid(linestyle=LINESTYLE)
     plt.xlabel("Epoch")
     plt.ylabel(ylabel)
 
@@ -52,11 +68,11 @@ def plot_orbit(x, Y, *, symbol, labels=(), title=None, xlabel=None, ylabel=None,
     plt.gca().xaxis.set_major_formatter(DateFormatter("$%d$ %b '$%y$\n$%H$:$%M$", usetex=False))
 
     for (i, y) in enumerate(Y):
-        plt.plot(x, y, linewidth=LINEWIDTH, label=f"${symbol}_{i + 1}$")
+        plt.plot(x, y, linewidth=LINEWIDTH_THIN, label=f"${symbol}_{i + 1}$")
 
     n = np.linalg.norm(np.array(Y), axis=0)
-    p = plt.plot(x, n, linewidth=LINEWIDTH, label=f"$\\pm||\\vec{{{symbol}}}||$")
-    plt.plot(x, -n, linewidth=LINEWIDTH, color=p[-1].get_color())
+    p = plt.plot(x, n, linewidth=LINEWIDTH_THIN, label=f"$\\pm||\\vec{{{symbol}}}||$")
+    plt.plot(x, -n, linewidth=LINEWIDTH_THIN, color=p[-1].get_color())
 
     colors = ["whitesmoke", "indianred", "burlywood", "cornflowerblue", "lightgreen"]
 
@@ -68,7 +84,7 @@ def plot_orbit(x, Y, *, symbol, labels=(), title=None, xlabel=None, ylabel=None,
         class_legend = [Patch(facecolor=colors[key], label=val) for key, val in c.CLASSES.items()]
         plt.gca().add_artist(plt.legend(handles=class_legend, loc="upper right"))
 
-    plt.grid(linewidth=LINEWIDTH, linestyle="--")
+    plt.grid(linewidth=LINEWIDTH_THIN, linestyle="--")
     plt.margins(x=0)
     plt.title(title)
     plt.xlabel(xlabel)
