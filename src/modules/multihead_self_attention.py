ebe1bb5ba342b3e8c3d44e53ad464766f6046ce9
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2022-01-16 00:54:15 +0100

    Correct input ordering

--- src/modules/multihead_self_attention.py
+++ src/modules/multihead_self_attention.py
@@ -5,7 +5,7 @@ class MultiheadSelfAttention(nn.Module):
 
     def __init__(self, embed_dim, num_heads):
         super().__init__()
-        self.attn = nn.MultiheadAttention(embed_dim, num_heads)
+        self.attn = nn.MultiheadAttention(embed_dim, num_heads, batch_first=True)
 
     def forward(self, x):
         return self.attn.forward(x, x, x)[0]
